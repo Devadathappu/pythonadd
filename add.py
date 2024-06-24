68f98fb108220b6bbe54e6f185130620b2f81703
Devadathappu <142972061+Devadathappu@users.noreply.github.com>
2024-06-24 10:08:30 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+# Define two numbers
+number1 = 10
+number2 = 20
+
+# Print the numbers
+print("The first number is:", number1)
+print("The second number is:", number2)
+
+# Add the two numbers
+sum_of_numbers = number1 + number2
+
+# Print the sum
+print("The sum of the two numbers is:", sum_of_numbers)
